39915c5a9ed95779349ea41e3f93934b511dc344
jisu3316 <jisu3316@naver.com>
2022-08-22 15:15:29 +0900

    init : initialize project

---
+++ src/main/java/com/jisu/JisuApplication.java
@@ -0,0 +1,14 @@
+package com.jisu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class JisuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JisuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jisu/JisuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JisuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
