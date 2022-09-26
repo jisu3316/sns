a5805a66d799c2aa8105984b33d80ca1a251f4e7
jisu3316 <jisu3316@naver.com>
2022-09-26 22:34:18 +0900

    포스트 삭제기능

--- src/main/java/com/jisu/controller/PostController.java
+++ src/main/java/com/jisu/controller/PostController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     private final PostService postService;
+
     @PostMapping
     public Response<Void> create(@RequestBody PostCreateRequest request, Authentication authentication) {
         postService.create(request.getTitle(), request.getBody(), authentication.getName());
@@ -26,5 +27,11 @@ public class PostController {
     public Response<PostResponse> modify(@PathVariable Integer postId, @RequestBody PostModifyRequest request, Authentication authentication) {
         Post post = postService.modify(request.getTitle(), request.getBody(), authentication.getName(), postId);
         return Response.success(PostResponse.fromPost(post));
+    }
+
+    @DeleteMapping("/{postId}")
+    public Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
+        postService.delete(authentication.getName(), postId);
+        return Response.success();
     }
 }
